a9f1c4dcba3f4ef52df99f47c10954e0d7d8d689
Amanda Worker <amandaworker@Amandas-MacBook-Pro.local>
2025-05-28 15:16:03 +0100

    Added geolocation boundary to cleaning step

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -37,6 +37,11 @@ def go(args):
     idx = df['price'].between(min_price, max_price)
     df = df[idx].copy()
 
+    # Set geolocation boundaries
+    logger.info("Setting geolocation boundaries")
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     # Convert last_review to datetime
     logger.info("Setting 'last_review' column to datetime dtype")
     df['last_review'] = pd.to_datetime(df['last_review'])
